6adb58e51ddb41b33ffac3106441c3157fe2ea7a
Mll <mllcms@163.com>
2024-09-25 22:08:02 +0800

    fix: 增加双工通信超时机制

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -2,33 +2,69 @@ pub mod config;
 pub mod connect;
 pub mod message;
 
-use std::io;
+use std::{
+    io,
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
+    time::Duration,
+};
 
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
+    task::JoinSet,
 };
 
 /// 全双工通信
-pub fn duplex(a: TcpStream, b: TcpStream) {
+pub fn duplex(a: TcpStream, b: TcpStream, timeout: Duration) {
     let (fr, fw) = a.into_split();
     let (tr, tw) = b.into_split();
-    tokio::spawn(forward(fr, tw));
-    tokio::spawn(forward(tr, fw));
+    let mut join_set = JoinSet::new();
+    let count = Arc::new(AtomicU64::new(0));
+    join_set.spawn(forward(fr, tw, Some(count.clone())));
+    join_set.spawn(forward(tr, fw, Some(count.clone())));
+
+    let mut prev = 0;
+    tokio::spawn(async move {
+        loop {
+            tokio::select! {
+                _ = join_set.join_next() => return,
+                _ = tokio::time::sleep(timeout) => {
+                    let curr = count.load(Ordering::Relaxed);
+                    if prev == curr {
+                        return eprintln!("Duplex Communication Timeout")
+                    }
+                    prev = curr
+                }
+            }
+        }
+    });
 }
 
 /// 数据转发
-pub async fn forward<R, W>(mut reader: R, mut writer: W) -> io::Result<()>
+pub async fn forward<R, W>(mut reader: R, mut writer: W, count: Option<Arc<AtomicU64>>) -> io::Result<()>
 where
     R: AsyncReadExt + Unpin,
     W: AsyncWriteExt + Unpin,
 {
     let mut buf = [0; 1024];
-    while let Ok(n) = reader.read(&mut buf).await {
-        if n == 0 {
-            break;
+    if let Some(c) = count {
+        while let Ok(n) = reader.read(&mut buf).await {
+            if n == 0 {
+                break;
+            }
+            c.fetch_add(1, Ordering::Relaxed);
+            writer.write_all(&buf[..n]).await?;
         }
-        writer.write_all(&buf[..n]).await?;
+    } else {
+        while let Ok(n) = reader.read(&mut buf).await {
+            if n == 0 {
+                break;
+            }
+            writer.write_all(&buf[..n]).await?;
+        }
     }
     writer.shutdown().await
 }
